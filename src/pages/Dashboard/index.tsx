2f5f5eb7c7e7de4ceefe6a88a9ebae42657826e2
Elen Magalhães <fromeleen@gmail.com>
2022-04-18 23:57:58 -0300

    Adicionado navegação entre  rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -80,7 +81,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {_repositories.map(repository => (
-          <a href="teste" key={repository.full_name}>
+          <Link to={`/repository/${repository.full_name}`} key={repository.full_name}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -91,7 +92,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
